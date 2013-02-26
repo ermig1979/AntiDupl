c20c625bb33bce4f0fc4ef3355dc1c312e51d54c
ermig1979 <ermig1979@7e5b2e0f-5202-417b-bd5d-7c31076a8210>
2013-02-26 08:26:58 +0000

    +added third party Simd project;

---
+++ src/3rd/Simd/SimdDefs.h
@@ -0,0 +1,100 @@
+/*
+* Simd Library.
+*
+* Copyright (c) 2011-2013 Yermalayeu Ihar.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy 
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+* copies of the Software, and to permit persons to whom the Software is 
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in 
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#ifndef __SimdDefs_h__
+#define __SimdDefs_h__
+
+#include "Simd/SimdConfig.h"
+
+#include <cstddef>
+
+#include <stdlib.h>
+#include <assert.h>
+
+#if defined(_MSC_VER) && defined(_MSC_FULL_VER)
+
+#if _MSC_VER >= 1600
+#include <stdint.h>
+#else
+#define SIMD_OWN_STDINT
+#endif
+
+#define SIMD_INLINE __forceinline
+
+#if !defined(SIMD_SSE2_DEPRECATE) && _MSC_VER >= 1300 
+#define SIMD_SSE2_ENABLE
+#endif
+
+#if !defined(SIMD_SSE42_DEPRECATE) && _MSC_VER >= 1500 
+#define SIMD_SSE42_ENABLE
+#endif
+
+#if !defined(SIMD_AVX_DEPRECATE) && _MSC_FULL_VER >= 160040219 
+#define SIMD_AVX_ENABLE
+#endif
+
+#if !defined(SIMD_AVX2_DEPRECATE) && _MSC_VER >= 1700 
+#define SIMD_AVX2_ENABLE
+#endif
+
+#elif defined(__GNUC__)
+
+#include <stdint.h>
+
+#define SIMD_INLINE inline __attribute__ ((always_inline))
+
+#if !defined(SIMD_SSE2_DEPRECATE) && defined(__SSE2__)
+#define SIMD_SSE2_ENABLE
+#endif
+
+#if !defined(SIMD_SSE42_DEPRECATE) && defined(__SSE4_2__)
+#define SIMD_SSE42_ENABLE
+#endif
+
+#if !defined(SIMD_AVX_DEPRECATE) && defined(__AVX__)
+#define SIMD_AVX_ENABLE
+#endif
+
+#if !defined(SIMD_AVX2_DEPRECATE) && defined(__AVX2__)
+#define SIMD_AVX2_ENABLE
+#endif
+
+#else
+
+#error This platform is unsupported!
+
+#endif
+
+#ifdef SIMD_SSE2_ENABLE
+#include <emmintrin.h>
+#endif
+
+#ifdef SIMD_SSE42_ENABLE
+#include <nmmintrin.h>
+#endif
+
+#if defined(SIMD_AVX_ENABLE) || defined(SIMD_AVX2_ENABLE)
+#include <immintrin.h>
+#endif
+
+#endif//__SimdDefs_h__
---
+++ src/3rd/Simd/SimdView.cpp
@@ -0,0 +1,118 @@
+/*
+* Simd Library.
+*
+* Copyright (c) 2011-2013 Yermalayeu Ihar.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy 
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+* copies of the Software, and to permit persons to whom the Software is 
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in 
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#include "Simd/SimdMath.h"
+#include "Simd/SimdView.h"
+
+namespace Simd
+{
+    size_t View::SizeOf(Format format)
+    {
+        switch(format)
+        {
+        case None:
+            return 0;
+        case Gray8:
+            return 1;
+		case Uv16:
+			return 2;
+        case Bgr24:
+            return 3;
+        case Bgra32:
+        case Int32:
+            return 4;
+        case Int64:
+            return 8;
+        default:
+            return 0;
+        }
+    }
+
+    View::View()
+    {
+        width = 0;
+        height = 0;
+        stride = 0;
+        format = None;
+        data = NULL;
+        _owner = false;
+    }
+
+    View::View(size_t w, size_t h, ptrdiff_t s, Format f, void* d)
+    {
+        width = w;
+        height = h;
+        stride = s;
+        format = f;
+        if(d)
+        {
+            data = (uchar*)d;
+            _owner = false;
+        }
+        else
+        {
+            data = (uchar*)Allocate(height*stride);
+            _owner = true;
+        }
+    }
+
+    View::View(size_t w, size_t h, Format f, void * d, size_t align)
+    {
+        width = w;
+        height = h;
+        format = f;
+        stride = AlignHi(width*SizeOf(format), align);
+        if(d)
+        {
+            data = (uchar*)AlignHi(d, align);
+            _owner = false;
+        }
+        else
+        {
+            data = (uchar*)Allocate(height*stride, align);
+            _owner = true;
+        }
+    }
+
+    View::~View()
+    {
+        if(_owner && data)
+        {
+            Free(data);
+        }
+    }
+
+    View View::Region(ptrdiff_t left, ptrdiff_t top, ptrdiff_t right, ptrdiff_t bottom) const
+    {
+        if(data != NULL && right >= left && bottom >= top)
+        {
+			left = Base::RestrictRange((int)left, 0, (int)width);
+			top = Base::RestrictRange((int)top, 0, (int)height);
+			right = Base::RestrictRange((int)right, 0, (int)width);
+			bottom = Base::RestrictRange((int)bottom, 0, (int)height);
+            return View(right - left, bottom - top, stride, format, data + top*stride + left*SizeOf(format));
+        }
+        else
+            return View();
+    }
+}
