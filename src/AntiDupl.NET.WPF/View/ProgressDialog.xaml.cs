e4dbfde142618f72b6e3fe1e29590b24c64ddbdc
gismo2004 <reumuellermarkus@gmail.com>
2023-03-05 19:15:48 +0100

    Close dialog after exeution finished

--- src/AntiDupl.NET.WPF/View/ProgressDialog.xaml.cs
+++ src/AntiDupl.NET.WPF/View/ProgressDialog.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using AntiDupl.NET.WPF.ViewModel;
 
@@ -17,11 +19,13 @@ public partial class ProgressDialog : Window
     protected override void OnClosing(CancelEventArgs e)
     {
         base.OnClosing(e);
+
+        //find out, if the close button was pressed or if the window was closed by win.close()
+        if (new StackTrace().GetFrames().FirstOrDefault(x => x.GetMethod() == typeof(Window).GetMethod("Close")) != null) return;
+
+        //if close button was pressed, cancel execution
         var vm = (ProgressDialogViewModel)DataContext;
-        if (vm.IsCancelled) return;
-        
-        //CancelCommand will close the window
-        e.Cancel = true;
         vm.CancelCommand.Execute(null);
+        e.Cancel = true;
     }
 }
